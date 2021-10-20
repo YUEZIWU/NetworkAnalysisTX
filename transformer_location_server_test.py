2a97373bd3731fab5c28b79cff052b461679afda
YUEZIWU <yw3054@columbia.edu>
2021-10-20 00:06:05 -0400

    update the data path

--- transformer_location_server_test.py
+++ transformer_location_server_test.py
@@ -1,8 +1,7 @@
 import os
 
 # locate to data folder
-data_folder_path = '/Users/yuezi/Google Drive/LAB/Africa-Model/transfomer_locations/demo_wards_yuezi'
-#'/mnt/nfs/eguide/projects/networkAnalysis/Kenya/networkDesign_results/'
+data_folder_path = '/mnt/nfs/eguide/projects/networkAnalysis/Kenya/networkDesign_results/'
 file_names = os.listdir(data_folder_path)
 
 for i, name in enumerate(file_names):
