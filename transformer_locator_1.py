2d9ad7aeb4542e7b8e0f302b62d21cc768d1e7af
YUEZIWU <yw3054@columbia.edu>
2021-11-11 22:10:19 -0500

    upload 2nd part code

--- transformer_locator_1.py
+++ transformer_locator_1.py
@@ -22,16 +22,27 @@ cate_file = pd.read_csv('wards_summary_mvlv_w_area_type.csv')
 def drop_mvs_from_finalGrid(df1,df2):
     indices = df1[df1.pt1.isin(df2.pt1) & df1.pt2.isin(df2.pt2)].index
     df1.drop(indices, inplace=True)
-    df = df1[['pt1','pt2']]
-    return df
+    return df1
+
+def find_dist(x1,y1,x2,y2):
+    return np.sqrt( (x2 - x1)**2 + (y2 - y1)**2)
 
 # modified DFS to find all structures of each transformer
-def dfs(visited, graph, node):
+# in dfs function, + find the farest distance between tx and structure
+def dfs(visited, graph, node, stpt, radTX):
     if node not in visited:
+        try:
+            if sum(lvs.pt1 == node) > 0:
+                structure = lvs.loc[lvs.pt1==node, 'geometry'].iloc[0].coords[0]
+            else:
+                structure = lvs.loc[lvs.pt2==node, 'geometry'].iloc[0].coords[1]
+            radTX.add(find_dist(stpt.x,stpt.y,structure[0],structure[1]))
+        except:
+            pass
         visited.add(node)
         neighbours = (graph.pt2[graph.pt1==node]).append(graph.pt1[graph.pt2==node])
         for neighbour in neighbours:
-            dfs(visited, graph, neighbour)
+            dfs(visited, graph, neighbour, stpt, radTX)
 
 tx_locations_geodf = gpd.GeoDataFrame()
 for i, name in enumerate(file_names):
@@ -66,11 +77,16 @@ for i, name in enumerate(file_names):
                 # find the structures connected to that transformer
                 lvs = drop_mvs_from_finalGrid(finalGrid,mvs)
                 result['NoStructure'] = None
+                result['radTX'] = None
                 for tx_o_id in result['id']:
                     tx_id = -tx_o_id-100
+                    ctpt = result.loc[result.id==tx_o_id, 'geometry'].item()
                     visited = set()
-                    dfs(visited, lvs, tx_id)
+                    radTX = set()
+                    dfs(visited, lvs, tx_id, ctpt, radTX)
                     result.loc[result.id == tx_o_id, 'NoStructure'] = len(visited)-1
+                    result.loc[result.id == tx_o_id, 'radTX'] = max(radTX)
+
             else: # when mvs is empty, choose the centriod as the transformer location
                 sub_grid_cons = None
                 sub_grid_cons = gpd.read_file(os.path.join(data_folder_path, name, sub_grid, "{}.shp".format(sub_grid)))
@@ -78,6 +94,11 @@ for i, name in enumerate(file_names):
                 result = gpd.GeoDataFrame(points,columns=['geometry'])
                 result['id'] = 0
                 result['NoStructure'] = len(sub_grid_cons)
+
+                radTX = set()
+                for con_n, sub_grid_con in sub_grid_cons.iterrows():
+                    radTX.add(find_dist(sub_grid_con['utm_x'], sub_grid_con['utm_y'], points.item().x, points.item().y))
+                result['radTX'] = max(radTX)
             # Assign info.
             result['county'] = county
             result['ward'] = ward
@@ -89,10 +110,10 @@ for i, name in enumerate(file_names):
             pass
     pts_results.index = range(len(pts_results))
     tx_locations_geodf = tx_locations_geodf.append(pts_results)
-    if i%20 ==0:
+    if i%10 ==0:
         print(i, 'time:', datetime.now() - now) # print the process steps of the script
         now = datetime.now()
 
-tx_locations_geodf.crs = {'init' :'epsg:4326'}
+tx_locations_geodf.crs = {'init' :'epsg:32737'}
 tx_locations_geodf.to_file(os.path.join(output_dir, "gridmodel_tx_locations_1.gpkg"), driver="GPKG")
 #tx_locations_geodf.to_file(os.path.join(output_dir, "gridmodel_tx_locations.shp"))
